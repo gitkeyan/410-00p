c28d90196a64001f219317d9bc8139e19fd048f9
YuanMe <yuanmeng.meng@mail.utoronto.ca>
2017-10-27 11:24:35 -0400

    Update wrapper.py
    Prints where the left hand side variables are.

--- pyminic-master/minic/wrapper.py
+++ pyminic-master/minic/wrapper.py
@@ -2,9 +2,35 @@ from pycparser import parse_file
 from pycparser.c_ast import *
 sys.path.extend(['.', '..'])
 
-from minic_ast import *
-from c_ast_to_minic import * 
 
+from c_ast_to_minic import * 
+from minic_ast import *
+
+
+class LHSPrinter(NodeVisitor):
+    def __init__(self):
+        self.varLst = set()  # all variables seen in the code
+        self.lhsVar = set()  # variables that have values assigned to it
+        
+    def visit_Assignment(self, assignment):
+        # The assignment node has a 'lvalue' field, we just
+        # want to show it here
+        varName = assignment.lvalue.name
+        self.varLst.add(varName)
+        self.lhsVar.add(varName) 
+        
+        print(assignment.lvalue)
+        print(assignment.rvalue)
+        
+        
+    def get_AllVar(self):
+        return self.varLst
+        
+        
+    def get_LHSVar(self):
+        return self.lhsVar
+            
+        
 
 # wrap raw C code into a simple 
 def makeDummyCFile(file):
@@ -29,4 +55,20 @@ def makeDummyCFile(file):
     f = open(fileName, 'w')
     f.write("int dummy(){\n" + newInput + "    return 0;\n}")
     f.close()
+    
+    return str(fileName)
 
+
+# write file name here
+file = r'write path to file here'
+
+dummyName = makeDummyCFile(file)
+
+
+
+ast = parse_file(dummyName)
+ast2 = ast
+visitor = LHSPrinter()
+visitor.visit(ast)
+print('Written Variables:')
+print(visitor.get_LHSVar())
