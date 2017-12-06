b9c7197b84e8881849be1d0c25c042e331cc5805
YuanMe <yuanmeng.meng@mail.utoronto.ca>
2017-12-06 13:24:21 -0500

    Add files via upload

---
+++ checkin6simp.py
@@ -0,0 +1,563 @@
+# example of how to run this script
+# python checkin_test.py /Users/abc/Desktop/project3inputs/checkin3_input1
+
+from pycparser import parse_file
+from pycparser.c_ast import *
+sys.path.extend(['.', '..'])
+
+from pyminicMaster.minic.minic_ast import *
+from pyminicMaster.c_ast_to_minic import * 
+import os
+
+import myfunctional_ast6simp as my
+
+class LHSPrinter(NodeVisitor):
+    def __init__(self):
+        self.varLst = set()  # all variables seen in the code
+        self.lhsVar = set()  # variables that have values assigned to it
+        self.declaredVar = set() # all declared variables
+
+    def __str__(self):
+        allVarTuple = ()
+        writtenVarLst = []
+
+        # use difference between all variables list and declared variable list 
+        # to find all non-declared variables
+        nonDeclaredVars = self.varLst.difference(self.declaredVar)
+        
+        #return "func block_function" + str(tuple(nonDeclaredVars)) + " return " + str(list(self.lhsVar))
+        return 'func block_function' +'(' + ','.join(map(str, nonDeclaredVars)) + ')' + " return " + '[' + ','.join(map(str, self.lhsVar)) + ']'
+
+    def visit_Decl(self, decl):
+        if decl.init is not None:
+            self.lhsVar.add(decl.name)
+            self.varLst.add(decl.name)
+            self.declaredVar.add(decl.name)
+            self.visit(decl.init)
+        else:
+            if not isinstance(decl.type, FuncDecl):
+                self.varLst.add(decl.name)
+                self.declaredVar.add(decl.name)
+
+    def visit_Assignment(self, assignment):
+        # get all left hand side variables as written variables
+        
+        if isinstance(assignment.lvalue, ID):
+            varName = assignment.lvalue.name
+            self.varLst.add(varName)
+            self.lhsVar.add(varName) 
+            
+        if isinstance(assignment.lvalue, ArrayRef):
+            arrayRef = assignment.lvalue
+            # set getArrayName to True to indicate that we want to add array into
+            # the write set
+            if isinstance(arrayRef.name, ID):
+                self.visit_ID(arrayRef.name, True)
+            elif isinstance(arrayRef.name, ArrayRef):
+                self.visit_ArrayRef(arrayRef.name, True) 
+            else:
+                self.visit(arrayRef.name)
+            self.visit(arrayRef.subscript)
+            
+        
+        # check if any right hand side variables have been written to
+        rval = assignment.rvalue
+        self.visit(rval)
+        
+        
+
+    def visit_BinaryOp(self, binaryOp):
+        self.visit(binaryOp.left)
+        self.visit(binaryOp.right)
+        
+    def visit_ID(self, id, getArrayName = False):
+        if getArrayName:
+            self.lhsVar.add(id.name)
+        self.varLst.add(id.name)
+        
+    def visit_FuncCall(self, funcCall):
+        if funcCall.args is not None:
+            for exprs, child in funcCall.args.children():
+                self.visit(child)
+                
+    def visit_ArrayRef(self, arrayRef, getArrayName = False):
+        # call the right visit to help add array modified into the write set
+        if isinstance(arrayRef.name, ID):
+            self.visit_ID(arrayRef.name, getArrayName)
+        elif isinstance(arrayRef.name, ArrayRef):
+            self.visit_ArrayRef(arrayRef.name, getArrayName)
+        else:
+            self.visit(arrayRef.name)
+        self.visit(arrayRef.subscript)
+    
+    def get_AllVar(self):
+        return self.varLst
+        
+        
+    def get_LHSVar(self):
+        return self.lhsVar
+        
+        
+    def get_DeclaredVar(self):
+        return self.declaredVar
+                
+# wrap raw C code into a simple 
+def makeDummyCFile(file):
+    
+    # read file
+    f = open(file, 'r')
+    input = f.read().split('\n')
+    f.close()
+    
+    # add indentation
+    newInput = "";
+    for line in input:
+        newInput += "    " + line + "\n"
+        
+    fileName, file_extension = os.path.splitext(file)  
+    fileName +=  'Dummy' + file_extension
+        
+    f = open(fileName, 'w')
+    f.write("int* block_function(){\n" + newInput + "    return 0;\n}")
+    f.close()
+    
+    return str(fileName)
+
+
+
+# ------------------------ Checkin 3 starts here -------------------------------
+
+
+# blockItemLst: list of statements to be read in the block
+# returnLst:    list of availiable bindings
+
+def minicToFunctional(ast, blockItemLst, returnLst, level = 0):
+
+    if isinstance(ast, FileAST):
+        statement = None
+        blockItems = ast.ext[0].body.block_items
+
+        statementCount = len(blockItems)
+        for i in range(statementCount):
+            statement = minicToFunctional(blockItems[i], blockItems[i+1:], [], level)
+            if statement is not None:
+                break
+                  
+        visitorF = LHSPrinter()
+        visitorF.visit(ast)
+        nonDeclaredVars = visitorF.varLst.difference(visitorF.declaredVar)
+        lhsVar = [var for var in visitorF.get_LHSVar()]
+        return my.FuncDef(nonDeclaredVars, statement, lhsVar)
+    
+    
+    # filters and convert declaration statement to let ... = ... in ...
+    if isinstance(ast, Decl):   
+        if ast.init is not None:
+            init = minicToFunctional(ast.init, [], returnLst, level + 1)
+            if not blockItemLst:
+                body = returnLst
+            else:
+                body = minicToFunctional(blockItemLst[0], blockItemLst[1:], returnLst + [ast.name], level + 1)
+            
+            return my.Let(ast.name, init, body, level)
+        else:
+            if not blockItemLst:
+                return returnLst
+            else:
+                return minicToFunctional(blockItemLst[0], blockItemLst[1:], returnLst, level)
+            
+    # convert assignment statement to let ... = ... in ...
+    if isinstance(ast, Assignment):
+        identifier = minicToFunctional(ast.lvalue, [], returnLst)
+
+        rv = minicToFunctional(ast.rvalue, [], returnLst, level + 1)
+
+        if not blockItemLst:
+            body = returnLst
+        else:
+            var = identifier
+            while isinstance(var, my.ArrayRef):
+                var = var.name
+                
+            body = minicToFunctional(blockItemLst[0], blockItemLst[1:], returnLst + [var], level + 1)
+        return my.Let(identifier, rv, body, level)
+        
+    if isinstance(ast, ID):
+        return my.ID(ast.name, level)
+        
+    if isinstance(ast, Constant):
+        return my.Constant(ast.value, level)
+
+    if isinstance(ast, Return):
+        return list(set(str(element) for element in returnLst))
+
+    if isinstance(ast, BinaryOp):
+        left = minicToFunctional(ast.left,[],returnLst)
+        right = minicToFunctional(ast.right,[],returnLst)
+        return my.BinaryOp(ast.op,left,right, level)
+    
+    if isinstance(ast, TernaryOp):
+        iftrue = minicToFunctional(ast.iftrue,[],returnLst, level + 1)
+        iffalse = minicToFunctional(ast.iffalse,[],returnLst, level + 1)
+        cond = minicToFunctional(ast.cond,[],returnLst)
+        return my.TernaryOp(cond, iftrue, iffalse, level)
+    
+    if isinstance(ast, FuncCall):
+        args = []
+        if ast.args is not None:
+            for arg in ast.args.exprs:
+                if isinstance(arg, Assignment):
+                    args += [minicToFunctional(arg, [], [arg.lvalue.name])]
+                else:
+                    args += [minicToFunctional(arg, [], returnLst)]
+        name = minicToFunctional(ast.name, blockItemLst, returnLst)
+        return my.FuncCall(name, args, level)
+
+    if isinstance(ast, ArrayRef):
+        # for future reference      
+        #if isinstance(ast.subscript, Assignment):
+        #    subscript = minicToFunctional(ast.subscript, [], [ast.subscript.lvalue.name])
+        #else:
+        name = minicToFunctional(ast.name, [], returnLst)
+        
+        subscript = minicToFunctional(ast.subscript, [], returnLst)
+        return my.ArrayRef(name, subscript, level);
+        
+    if isinstance(ast, UnaryOp):
+        # for future reference 
+        #if isinstance(ast.expr, Assignment):
+        #    expr = minicToFunctional(ast.expr, [], [ast.expr.lvalue.name])
+        #else:
+        expr = minicToFunctional(ast.expr, [], returnLst)
+        return my.UnaryOp(ast.op, expr, level)
+    
+    
+    if isinstance(ast, ExprList):
+        # convert something like a[1,2,b[1]] to functional programming
+        exprs = [minicToFunctional(expr, [], returnLst) for expr in ast.exprs]
+        return my.ExprList(exprs)
+        
+    # ---------------- Checkin 4 starts here -----------------------------------
+    if isinstance(ast, If):
+        # get all the written variables
+        visitor1 = LHSPrinter()
+        visitor1.visit(ast.iftrue)
+        
+        # determine all written variables in if and else
+        if ast.iffalse is None:
+            allLhs = list(visitor1.get_LHSVar())
+        else:
+            visitor2 = LHSPrinter()
+            visitor2.visit(ast.iffalse)
+            
+            # add the variables together
+            allLhs = list( visitor1.get_LHSVar().union(visitor2.get_LHSVar()) )
+        
+        iftrue = minicToFunctional(ast.iftrue,[],allLhs, level + 2)  # returnLst) 
+        
+        if ast.iffalse is None:
+            iffalse = my.ReturnTuples(tuple(allLhs), level + 2)    # make the else statement when it doesn't exist
+        else:
+            # convert else statement to Let
+            iffalse = minicToFunctional(ast.iffalse,[],allLhs, level + 2)  # returnLst)
+        cond = minicToFunctional(ast.cond,[],[])   # returnLst)
+        
+        ternary = my.TernaryOp(cond, iftrue, iffalse, level + 1)
+        
+
+        if not blockItemLst:
+            body = my.ReturnTuples(returnLst, level + 1) #returnLst
+        else:
+            body = minicToFunctional(blockItemLst[0], blockItemLst[1:], returnLst + allLhs, level + 1)
+        
+        letStatement = my.Let(tuple(allLhs), ternary, body, level)
+        
+        return letStatement
+        #return my.TernaryOp(cond, ast.iftrue.block_items[0].lvalue.name, iffalse)
+    if isinstance(ast, Block):
+        statement = None
+        blockItems = ast.block_items + [Return([])]
+
+        statementCount = len(blockItems)
+        for i in range(statementCount):
+            statement = minicToFunctional(blockItems[i], blockItems[i+1:], returnLst, level)
+            if statement is not None:
+                break
+        return statement
+    
+    
+    # ------------------------ Checkin 6 starts here ---------------------------        
+    if isinstance(ast, While):
+        visitorF = LHSPrinter()
+        visitorF.visit(ast)
+        nonDeclaredVars = visitorF.varLst.difference(visitorF.declaredVar)
+        lhsVar = tuple([var for var in visitorF.get_LHSVar()])
+        
+        assignedStatements = minicToFunctional(ast.stmt, [], [], level + 3)
+    
+        recursiveCall = my.LetrecCall('loop0', lhsVar, level + 3)        
+        recursiveLet = my.Let(lhsVar, assignedStatements, recursiveCall, level + 2)
+        
+        cond = minicToFunctional(ast.cond, [], [])
+        ifStatement = my.TernaryOp(cond, recursiveLet, lhsVar, level + 1)        
+
+        newReturnLst = returnLst + list(lhsVar)
+        body = minicToFunctional(blockItemLst[0], blockItemLst[1:], newReturnLst, level + 1)
+        statement = my.Letrec('loop0', lhsVar, ifStatement, body, level)
+        return statement
+    
+    if isinstance(ast, DoWhile):
+        # convert to statments + while with statements
+        
+        minicWhile = While(ast.cond, ast.stmt)
+        newBlockItemLst = ast.stmt.block_items + [minicWhile] + blockItemLst
+        return minicToFunctional(newBlockItemLst[0], newBlockItemLst[1:], returnLst, level)
+        
+    
+    if isinstance(ast, For):
+        # convert to init + while with (statements + next)
+        
+        newStmt = Block(ast.stmt.block_items + [ast.next])
+        minicWhile = While(ast.cond, newStmt)
+        newBlockItemLst = [ast.init, minicWhile] + blockItemLst
+        
+        return minicToFunctional(newBlockItemLst[0], newBlockItemLst[1:], returnLst, level)
+    
+    return None
+
+
+
+import copy
+def simplify(ast):
+    newAst = copy.deepcopy(ast)
+    
+    if isinstance(newAst, my.FuncDef):
+        newAst.body = simplify(newAst.body)
+        return newAst
+        
+    if isinstance(newAst, my.Let):        
+        
+        if isinstance(newAst.ident, my.ID) and isinstance(newAst.assignedExpr, my.Constant):
+            
+            varName = str(newAst.ident).strip()
+            
+            val = newAst.assignedExpr
+            newAst = replaceVar(newAst.bodyExpr, varName, val)
+            
+            # if variable should not be modified
+            if newAst is None:
+                newAst = copy.deepcopy(ast)
+                newAst.bodyExpr = simplify(newAst.bodyExpr)
+            else:
+                newAst = simplify(newAst);
+            return newAst
+            
+        if isinstance(newAst.ident, my.ArrayRef):  
+            newAst.bodyExpr = simplify(newAst.bodyExpr)
+            return newAst
+
+        newAst.assignedExpr = simplify(newAst.assignedExpr)
+        newAst.bodyExpr = simplify(newAst.bodyExpr)
+        
+
+        return newAst
+        
+    if isinstance(newAst, my.TernaryOp):
+        newAst.iftrue = simplify(newAst.iftrue) 
+        newAst.iffalse = simplify(newAst.iffalse)
+        
+        return newAst
+        
+    if isinstance(newAst, my.Letrec):
+        newAst.bodyExpr = simplify(newAst.bodyExpr)
+    
+    return newAst
+
+def replaceVar(ast, varName, val):
+    
+    if isinstance(ast, my.FuncDef):
+        newAst = copy.deepcopy(ast)
+        newAst.body = replaceVar(newAst.body, varName, val) 
+
+        # Check if variable to be replace exist in a loop
+        if newAst.body is None:
+            return None
+        else:
+            return newAst
+    
+    if isinstance(ast, my.Let):
+        newAst = copy.deepcopy(ast)
+        if isinstance(newAst.ident, str):
+            newAst.ident = my.ID(newAst.ident.strip(), 0)
+        
+        if isinstance(newAst.ident, my.ID) or isinstance(newAst.ident, my.ArrayRef):
+            
+            if isinstance(newAst.ident, my.ArrayRef):
+                newAst.ident = replaceVar(newAst.ident, varName, val)
+                
+                # Check if variable to be replace exist in a loop
+                if newAst.ident is None:
+                    return None
+            
+            newAst.assignedExpr = replaceVar(newAst.assignedExpr, varName, val)
+            if newAst.assignedExpr is None:
+                return None
+
+            
+            # if the name of variable is not the one that need to be replaced,
+            # check if 
+            if (str(newAst.ident).strip() != varName):
+                newAst.bodyExpr = replaceVar(newAst.bodyExpr, varName, val) 
+                
+                if newAst.bodyExpr is None:
+                    return None
+                
+            return newAst
+        
+                
+        if isinstance(newAst.ident, list) or isinstance(newAst.ident, tuple):
+            newAst.assignedExpr = replaceVar(newAst.assignedExpr, varName, val)
+        
+            if newAst.assignedExpr is None:
+                return None
+                
+        return newAst
+        
+    if isinstance(ast, my.ID):
+        newAst = copy.deepcopy(ast)
+        # replace variable if the name are the same
+        if str(ast).strip() == varName:
+            newAst = copy.deepcopy(val)
+            newAst.level = ast.level 
+        return newAst
+
+        
+    if isinstance(ast, my.ReturnTuples):
+        newAst = copy.deepcopy(ast)
+        
+        if isinstance(newAst.exprs, tuple):
+            newAst.exprs = list(newAst.exprs)
+        
+        for i in range(len(ast.exprs)):
+            if isinstance(newAst.exprs[i], str) and (newAst.exprs[i] == str(varName).strip()):
+                newAst.exprs[i] = copy.deepcopy(val)
+                newAst.exprs[i].level = 0 
+        return newAst
+        
+        
+    if isinstance(ast, my.BinaryOp):
+        newAst = copy.deepcopy(ast)
+        
+        newAst.left = replaceVar(newAst.left, varName, val)
+        
+        if newAst.left is None:
+            return None
+        
+        newAst.right = replaceVar(newAst.right, varName, val)
+        
+        if newAst.right is None:
+            return None
+        return newAst
+        
+    if isinstance(ast, my.TernaryOp):
+        newAst = copy.deepcopy(ast)
+        newAst.cond = replaceVar(newAst.cond, varName, val)
+        newAst.iftrue = replaceVar(newAst.iftrue, varName, val)
+        
+        if newAst.iftrue is None:
+            return None
+        
+        newAst.iffalse = replaceVar(newAst.iffalse, varName, val)
+        
+        if newAst.iffalse is None:
+            return None
+        
+        return newAst
+        
+    if isinstance(ast, my.FuncCall):
+        newAst = copy.deepcopy(ast)
+        for i in range(len(newAst.args)):
+            if isinstance(newAst.args[i], str) and (newAst.args[i] == str(varName).strip()):
+                newAst.args[i] = copy.deepcopy(val)
+                newAst.args[i].level = 0
+                
+            else:
+                newAst.args[i] = replaceVar(newAst.args[i], varName, val)
+        
+        return newAst
+
+    if isinstance(ast, my.ArrayRef):
+        newAst = copy.deepcopy(ast)
+        if not isinstance(newAst.name, my.ID):
+            newAst.name = replaceVar(newAst.name, varName, val)
+        
+        newAst.subscript = replaceVar(newAst.subscript, varName, val)
+        return newAst
+
+    if isinstance(ast, my.UnaryOp):
+        newAst = copy.deepcopy(ast)
+        newAst.expr = replaceVar(newAst.expr, varName, val)
+        return newAst
+
+    if isinstance(ast, my.ExprList):
+        newAst = copy.deepcopy(ast)
+        for i in range(len(newAst.exprs)):
+            if isinstance(newAst.exprs[i], str) and (newAst.exprs[i] == str(varName).strip()):
+                newAst.exprs[i] = copy.deepcopy(val)
+                newAst.exprs[i].level = 0
+                
+            else:
+                newAst.exprs[i] = replaceVar(newAst.exprs[i], varName, val)
+
+        return newAst
+        
+    if isinstance(ast, my.Letrec):
+        
+        # return back a None to indicated that the variable should not be simplified because it is used in a loop
+        if varName in ast.args:
+            return None
+        else:
+            newAst = copy.deepcopy(ast)
+            newAst.assignedExpr = replaceVar(newAst.assignedExpr, varName, val)
+            if newAst.assignedExpr is None:
+                return None
+            
+            newAst.bodyExpr = replaceVar(newAst.bodyExpr, varName, val)
+            
+            if newAst.bodyExpr is None:
+                return None
+            return newAst
+        
+        
+   
+    return ast
+
+
+inputFile = sys.argv[1]
+dummyName = makeDummyCFile(inputFile)
+
+ast = parse_file(dummyName)
+
+
+ast1 = transform(ast)
+
+visitor = LHSPrinter()
+visitor.visit(ast1)
+
+print("Input:\n")
+f = open(dummyName, 'r')
+input = f.read()
+print(input)
+f.close()
+
+
+print("\n\n----- Output: -----\n")
+
+ast2 = transform(ast)
+
+functionalAST = minicToFunctional(ast2, [], [], 1)
+print(functionalAST)
+
+print('\n\n --------- Simplified ----------\n')
+simplifiedAST = simplify(functionalAST)
+print(str(simplifiedAST))
